6316bfc1ab755ab87dba214f82809f1efa929fa8
Maxim Tishchenko <qwertmax@gmail.com>
2020-01-24 16:12:53 +0300

    dbtables

--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+)
+
+// ContextInjector - inject DB
+type ContextInjector struct {
+	ctx context.Context
+	h   http.Handler
+}
+
+func (ci *ContextInjector) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ci.h.ServeHTTP(w, r.WithContext(ci.ctx))
+}
 
 func main() {
 	db := NewStorage()
 	createUserTable(db.DB, "users")
 	createUserTable(db.DB, "address")
 
-	fmt.Printf("%#v\n", db)
+	host, ok := os.LookupEnv("APP_HOST")
+	if !ok {
+		host = "localhost"
+		log.Print("using default host `localhost`")
+	}
+	port, ok := os.LookupEnv("APP_PORT")
+	if !ok {
+		port = "8080"
+		log.Print("using default port `80`")
+	}
+
+	// create base Context
+	ctx := context.WithValue(context.Background(), "db", db.DB)
+
+	// users handlers
+	http.Handle("/", &ContextInjector{ctx, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})})
+
+	log.Fatal(http.ListenAndServe(host+":"+port, nil))
 }
