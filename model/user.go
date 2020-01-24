3ff5a0adbed9ab5a7b110f9576ac55f024037d40
Maxim Tishchenko <qwertmax@gmail.com>
2020-01-24 12:55:55 +0300

    init

---
+++ model/user.go
@@ -0,0 +1,9 @@
+package model
+
+// User ...
+type User struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
